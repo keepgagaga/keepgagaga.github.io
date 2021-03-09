f2c3b64c0b2aee9436a21a4fd8bf6827849b3d1b
saulzh <z@ruiruizhang.com>
2021-03-09 20:28:17 +0800

    init

---
+++ src/react/App.js
@@ -0,0 +1,14 @@
+import { React } from "react";
+import "./App.css";
+
+class App extends Comment {
+    render() {
+        return (
+            <div className="App"> 
+                <h1>hello react</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
